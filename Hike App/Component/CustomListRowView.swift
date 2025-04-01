ec526e31cacb4a0238e1faf9423bf6aec541317a
Ghenadie Isacenco <isacenco@gmail.com>
2025-04-01 18:56:20 +0200

    SwiftUI List and Labeled Content

---
+++ Hike App/Component/CustomListRowView.swift
@@ -0,0 +1,66 @@
+//
+//  CustomListRowView.swift
+//  Hike App
+//
+//  Created by Ghenadie Isacenco on 1/4/25.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    // MARK: - PROPERTIES
+    
+    @State var rowLabel: String
+    @State var rowIcon: String
+    @State var rowContent: String? = nil
+    @State var rowTintColor: Color
+    @State var rowLinkLabel: String? = nil
+    @State var rowLinkDestination: String? = nil
+    
+    var body: some View {
+        LabeledContent {
+            // Contect
+            if rowContent != nil {
+                Text(rowContent!)
+                    .foregroundColor(.primary)
+                    .fontWeight(.heavy)
+            } else if (rowLinkLabel != nil && rowLinkDestination != nil) {
+                Link(rowLinkLabel!, destination: URL(string: rowLinkDestination!)!)
+                    .foregroundColor(.pink)
+                    .fontWeight(.heavy)
+            } else {
+                /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+            }
+        } label: {
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(rowTintColor)
+                    Image(systemName: rowIcon)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                }
+                Text(rowLabel)
+            }
+        }
+    }
+}
+
+#Preview {
+    List {
+        CustomListRowView(
+            rowLabel: "Designer",
+            rowIcon: "paintpalette",
+            rowContent: "John Doe",
+            rowTintColor: .pink)
+        CustomListRowView(
+            rowLabel: "Website",
+            rowIcon: "globe",
+            rowContent: nil,
+            rowTintColor: .pink,
+            rowLinkLabel: "Credo Academy",
+            rowLinkDestination: "https://credo.academy"
+        )
+    }
+}
