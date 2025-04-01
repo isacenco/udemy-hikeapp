f708f62c0c96636d1239caede87d064778f2d196
Ghenadie Isacenco <isacenco@gmail.com>
2025-04-01 12:12:26 +0200

    SwiftUI Card Component + Card Header View

---
+++ Hike App/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Hike App
+//
+//  Created by Ghenadie Isacenco on 1/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike App/Utility/ColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ColorExtension.swift
+//  Hike App
+//
+//  Created by Ghenadie Isacenco on 1/4/25.
+//
+
+import SwiftUI
+import Foundation
+
+extension Color {
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmonLight = Color("ColorSalmonLight")
+}
